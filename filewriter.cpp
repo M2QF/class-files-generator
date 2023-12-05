4a7bb7ad6714e5162bc8d975f1917b78c2924893
Thomas Roiseux <thomas.roiseux@outlook.com>
2023-12-05 16:29:17 +0100

    Added doxy comments to file

--- filewriter.cpp
+++ filewriter.cpp
@@ -1,1 +1,13 @@
+/**
+ * @file filewriter.cpp
+ * @author AiglonDore
+ * @brief Implements {@link filewriter.h}.
+ * @warning This file is yet empty as everything is in the header file.
+ * @version 0.1
+ * @date 2023-12-05
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
 #include "filewriter.h"
