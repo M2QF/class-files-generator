91f13ebc2035bd7295c76056e42a14791c97c9f3
Thomas Roiseux <thomas.roiseux@outlook.com>
2023-12-05 10:31:03 +0100

    Added constructor

--- corewriter.h
+++ corewriter.h
@@ -4,6 +4,15 @@ class CoreWriter :
     public FileWriter
 {
 public: 
+    //constructor
+    CoreWriter(const CoreWriter&) = delete;
+    CoreWriter(const std::string& classname, const std::string& filename) : FileWriter(classname, filename) {};
+    //destructor
+    virtual ~CoreWriter() = default;
+
+    CoreWriter& operator=(const CoreWriter&) = delete;
+    CoreWriter(const CoreWriter&) = delete;
+
     //method that will create and write the .cpp file
     virtual void write() override;
 };
