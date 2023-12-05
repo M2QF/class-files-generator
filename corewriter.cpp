bd0100e419124e174f320673e1c4739097fde64a
Sarahnour Ghaith <sarahnour.ghaith@ensiie.fr>
2023-12-05 10:14:05 +0100

    wrote files corewriter

--- corewriter.cpp
+++ corewriter.cpp
@@ -1,1 +1,25 @@
 #include "corewriter.h"
+#include <fstream>
+#include <stdexcept>
+#include <iostream>
+
+using namespace std;
+void CoreWriter::write(){
+    //create the .cpp file
+    ofstream file(FileWriter::getFilename() + ".cpp");
+    if(!file.is_open()){
+        throw runtime_error("Error creating file " + FileWriter::getFilename() + ".cpp");
+    }
+
+    cout << "Writing " << FileWriter::getFilename() << ".cpp" << endl;
+    //write the include statements
+    file << "#include \"" << FileWriter::getClassname() << ".h\"" << endl;
+    //write the default constructor
+    file << FileWriter::getClassname() << "::" << FileWriter::getClassname() << "(){" << endl;
+    file << "}" << endl;
+    //write the default destructor 
+    file << FileWriter::getClassname() << "::~" << FileWriter::getClassname() << "(){" << endl;
+    file << "}" << endl;
+    cout << "Done writing " << FileWriter::getFilename() << ".cpp" << endl;
+    file.close();
+}
--- corewriter.h
+++ corewriter.h
@@ -3,5 +3,8 @@
 class CoreWriter :
     public FileWriter
 {
+public: 
+    //method that will create and write the .cpp file
+    virtual void write() override;
 };
 
