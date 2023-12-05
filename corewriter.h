a4b4a5bfede0b173a6c01fa13b389fe9f7f68843
Thomas Roiseux <thomas.roiseux@outlook.com>
2023-12-05 10:32:44 +0100

    Removed duplicate

--- corewriter.h
+++ corewriter.h
@@ -5,13 +5,13 @@ class CoreWriter :
 {
 public: 
     //constructor
+    CoreWriter() = delete;
     CoreWriter(const CoreWriter&) = delete;
     CoreWriter(const std::string& classname, const std::string& filename) : FileWriter(classname, filename) {};
     //destructor
     virtual ~CoreWriter() = default;
 
     CoreWriter& operator=(const CoreWriter&) = delete;
-    CoreWriter(const CoreWriter&) = delete;
 
     //method that will create and write the .cpp file
     virtual void write() override;
