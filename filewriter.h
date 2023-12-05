050523be3a4f3ca6c2dd12da3a280286b7794f26
Thomas Roiseux <thomas.roiseux@outlook.com>
2023-12-05 09:45:58 +0100

    Wrote base class FileWriter

--- filewriter.h
+++ filewriter.h
@@ -1,5 +1,18 @@
 #pragma once
+
+#include <string>
+
 class FileWriter
 {
+public:
+	FileWriter() = delete;
+	FileWriter(const std::string& classname, const std::string& filename) : classname(classname), filename(filename) {};
+
+	virtual ~FileWriter() = default;
+
+	virtual void write() = 0;
+private:
+	std::string classname;
+	std::string filename;
 };
 
