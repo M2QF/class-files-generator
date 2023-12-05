d9a6ac75cb387c23bbb5bf2d439243ebe8ce7f2c
Thomas Roiseux <thomas.roiseux@outlook.com>
2023-12-05 10:08:56 +0100

    Wrote header writer

--- headerwriter.cpp
+++ headerwriter.cpp
@@ -1,1 +1,39 @@
 #include "headerwriter.h"
+
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+void HeaderWriter::write()
+{
+	cout << "Writing header file " << getFilename() << ".h" << endl;
+	ofstream file(getFilename() + ".h");
+
+	if (!file.is_open())
+	{
+		throw runtime_error("Could not open file " + getFilename() + ".h");
+	}
+	else
+	{
+		cout << "Writing header file " << getFilename() << ".h" << endl;
+	}
+
+	file << "#pragma once" << endl;
+
+	file << "class " << getClassname() << endl;
+	file << "{" << endl;
+
+	file << "public:" << endl;
+	file << "\t" << getClassname() << "();" << endl;
+	file << "\t" << "~" << getClassname() << "();" << endl;
+
+	file << "private:" << endl;
+	file << "\t" << endl;
+
+	file << "};" << endl;
+
+	cout << "Finished writing header file " << getFilename() << ".h" << endl;
+
+	file.close();
+}
--- headerwriter.h
+++ headerwriter.h
@@ -1,7 +1,16 @@
 #pragma once
 #include "filewriter.h"
+
 class HeaderWriter :
     public FileWriter
 {
+    HeaderWriter(const HeaderWriter&) = delete;
+    HeaderWriter& operator=(const HeaderWriter&) = delete;
+
+public:
+    explicit HeaderWriter(const std::string& classname, const std::string& filename) : FileWriter(classname, filename) {};
+    virtual ~HeaderWriter() = default;
+
+    virtual void write() override;
 };
 
