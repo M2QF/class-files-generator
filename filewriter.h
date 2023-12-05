8716db43f37c21dc4ff40c336e3fb0b0254c27e5
Thomas Roiseux <thomas.roiseux@outlook.com>
2023-12-05 16:32:14 +0100

    Added doxy comments to file

--- filewriter.h
+++ filewriter.h
@@ -21,17 +21,54 @@
 class FileWriter
 {
 public:
+	/**
+	 * @brief Construct a new File Writer object
+	 * @warning Deleted
+	 * 
+	 */
 	FileWriter() = delete;
+
+	/**
+	 * @brief Construct a new File Writer object
+	 * 
+	 */
 	FileWriter(const std::string& classname, const std::string& filename) : classname(classname), filename(filename) {};
 
+	/**
+	 * @brief Destroy the File Writer object
+	 * 
+	 */
 	virtual ~FileWriter() = default;
 
+	/**
+	 * @brief Writes the file
+	 * 
+	 */
 	virtual void write(std::mutex* = nullptr) = 0;
 
+	/**
+	 * @brief Get the Classname object
+	 * 
+	 * @return const std::string& class name
+	 */
 	const std::string& getClassname() const { return classname; }
+	/**
+	 * @brief Get the Filename object
+	 * 
+	 * @return const std::string& file name
+	 */
 	const std::string& getFilename() const { return filename; }
+
 private:
+	/**
+	 * @brief class name
+	 * 
+	 */
 	std::string classname;
+	/**
+	 * @brief file name
+	 * 
+	 */
 	std::string filename;
 };
 
