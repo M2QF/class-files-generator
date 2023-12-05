c8e8862c78a546939518754d8998eb846ba5f052
Thomas Roiseux <thomas.roiseux@outlook.com>
2023-12-05 16:24:32 +0100

    Added blank lines

--- corewriter.cpp
+++ corewriter.cpp
@@ -4,35 +4,49 @@
 #include <stdexcept>
 
 using namespace std;
-void CoreWriter::write(std::mutex *coutMutex){
+
+void CoreWriter::write(std::mutex *coutMutex)
+{
     //create the .cpp file
     ofstream file(FileWriter::getFilename() + ".cpp");
-    if(!file.is_open()){
+    if(!file.is_open())
+    {
         throw runtime_error("Error creating file " + FileWriter::getFilename() + ".cpp");
     }
-    if (coutMutex != nullptr) {
+    if (coutMutex != nullptr)
+    {
 		coutMutex->lock();
 	}
+
     cout << "Writing " << FileWriter::getFilename() << ".cpp" << endl;
-    if (coutMutex != nullptr) {
+
+    if (coutMutex != nullptr)
+    {
         coutMutex->unlock();
     }
+
     //write the include statements
     file << "#include \"" << FileWriter::getClassname() << ".h\"" << endl;
     file << endl;
+
     //write the default constructor
     file << FileWriter::getClassname() << "::" << FileWriter::getClassname() << "(){" << endl;
     file << "}" << endl;
     file << endl;
+
     //write the default destructor 
     file << FileWriter::getClassname() << "::~" << FileWriter::getClassname() << "(){" << endl;
     file << "}" << endl;
-    if (coutMutex != nullptr) {
+
+    if (coutMutex != nullptr)
+    {
 		coutMutex->lock();
 	}
     cout << "Done writing " << FileWriter::getFilename() << ".cpp" << endl;
-    if (coutMutex != nullptr) {
+    if (coutMutex != nullptr)
+    {
 		    coutMutex->unlock();
 	}
+    
     file.close();
 }
